d5768f23b0efb1d8387a61421779b8d514e135e1
TonyWhiteb <tonybai2016@gmail.com>
2019-03-08 13:51:45 -0600

    WorkingOnFileListCtrl

--- BasicClass/FileLIstCtrl.py
+++ BasicClass/FileLIstCtrl.py
@@ -6,6 +6,38 @@ class FileCtrl(wx.ListCtrl):
     
         super(FileCtrl, self).__init__(*args, **kwargs)
 
+        self.currRow = None
+        
+        self.Bind(wx.EVT_LEFT_DOWN, self.OnFindCurrentRow)
+        
+
+    
+    def OnFindCurrentRow(self,event):
+        if (self.currRow is not None):
+            self.Select(self.currRow, False)
+
+        row,_ignoredFlags = self.HitTest(event.GetPosition())
+
+        self.currRow = row
+        self.Select(row)
+    def OnRightDown(self,event):
+
+        menu = wx.Menu()
+        menuItem = menu.Append(-1, 'Delet This File')
+
+        self.Bind(wx.EVT_MENU, self.OnDeleteRow, memuItem)
+    def OnDeleteRow(self, event):
+
+        if(self.currRow >= 0):
+
+            assert(self.numEntries == len(self.entriesList))
+
+            allSelectedRowData = self.GetAllSelectedRowData()
+
+    def GetCurrRow(self):
+        return self.currRow
+
+
 
 
 class FilePanel(wx.Panel):
